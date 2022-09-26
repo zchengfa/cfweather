21e63341289c5725006ec608521d55c368fb7056
zchengfa <1522977814@qq.com>
2022-09-26 09:43:15 +0800

    init

---
+++ pages/index/weatherBG.js
@@ -0,0 +1,26 @@
+const weatherBgImage = [
+  {
+    weather:"多云",
+    bg:"https://gd-hbimg.huaban.com/0330b06c2575df4e04de2d87f9ee1dd72c363aaa419d7-oj2ke4_fw658"
+  },
+  {
+    weather:"晴",
+    bg:"https://gd-hbimg.huaban.com/b15bee9fb11e5d3edd7eff728a378331a7fb2f9c317248-snSR0M_fw658"
+  },
+  {
+    weather:"雨",
+    bg:"https://hbimg.huabanimg.com/9b6d48f01dd2a086a0bab357791b06fb53c93f463f9f1-io6d8i_fw658/format/webp"
+  },
+  {
+    weather:"雪",
+    bg:"https://gd-hbimg.huaban.com/7407ab4061ad70bc6926e17925f9e7560936c08393924-fvyJY3_fw658"
+  },
+  {
+    weather:"default",
+    bg:"https://gd-hbimg.huaban.com/2d65b0da97498b3f97dc1618c6d2379dd8088d9e5bae90-BXduiN_fw658"
+  }
+]
+
+module.exports = {
+  weatherBgImage
+}
